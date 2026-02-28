358141fa194f19b814f017fce76baf87679813bc
3DA <3da@darijanavric.dev>
2026-02-28 09:30:19 +0100

    feat: add isFixed to ProposedExpenseData interface

--- backend/src/approval/interfaces/proposed-expense-data.interface.ts
+++ backend/src/approval/interfaces/proposed-expense-data.interface.ts
@@ -14,6 +14,7 @@ export interface ProposedExpenseData {
     installmentCount?: number | null;
     paymentMonth?: number | null;
     paidByUserId?: string | null;
+    isFixed?: boolean | null;
 }
 
 /**
